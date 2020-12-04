561545a974e8b003128dcce2ff96d879d05800e1
CS-Sauvaj <hugosimonyjungo@aol.fr>
2020-12-04 18:22:16 +0100

    Fixed Main

--- src/fr/hugosimony/aoc2020/Main.java
+++ src/fr/hugosimony/aoc2020/Main.java
@@ -3,6 +3,7 @@ package fr.hugosimony.aoc2020;
 import fr.hugosimony.aoc2020.day1.ProblemsDay1;
 import fr.hugosimony.aoc2020.day2.ProblemsDay2;
 import fr.hugosimony.aoc2020.day3.ProblemsDay3;
+import fr.hugosimony.aoc2020.day4.ProblemsDay4;
 
 public class Main {
 	
@@ -19,10 +20,13 @@ public class Main {
 		System.out.println("Problem 2 : Solution = " + ProblemsDay1.Problem2());
 		System.out.println("------------------ Day 2 ------------------");
 		System.out.println("Problem 1 : Solution = " + ProblemsDay2.Problem1());
-		System.out.println("Problem 1 : Solution = " + ProblemsDay2.Problem2());
+		System.out.println("Problem 2 : Solution = " + ProblemsDay2.Problem2());
 		System.out.println("------------------ Day 3 ------------------");
 		System.out.println("Problem 1 : Solution = " + ProblemsDay3.Problem1());
-		System.out.println("Problem 1 : Solution = " + ProblemsDay3.Problem2());
+		System.out.println("Problem 2 : Solution = " + ProblemsDay3.Problem2());
+		System.out.println("------------------ Day 4 ------------------");
+		System.out.println("Problem 1 : Solution = " + ProblemsDay4.Problem1());
+		System.out.println("Problem 2 : Solution = " + ProblemsDay4.Problem2());
 		
 	}
 	
